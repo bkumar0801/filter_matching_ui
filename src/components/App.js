de5e5a2c5cd5ea2eb68fa67b8f96af4a28a74338
Bijendra Kumar <kumarbijendra@gmail.com>
2018-05-10 19:19:12 +0200

    Bijendra | Refactor: separate profiles/profile component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
  import React from 'react';
  import '../style/App.css';
  import axios from "axios";
- import ToggleButton from "./ToggleButton"
- import RangeSlider from "./RangeSlider"
- import no_photo from "../image/no-user-image.png"
+ import ToggleButton from "./ToggleButton";
+ import RangeSlider from "./RangeSlider";
+ import Profiles from "./Profiles";
+ import no_photo from "../image/no-user-image.png";
 
  class App extends React.Component {
    constructor(props) {
@@ -138,52 +139,6 @@
     </div>
     );
    }
- }
-
- class Profiles extends React.Component {
-   render() {
-     if(this.props.profiles.length !== 0) {
-      return (
-       <div className="feed">
-         {this.props.profiles.map(c => <Profile displayName={c.displayName} job={c.job}
-         age = {c.age} height = {c.height} photo = {c.photo} religion = {c.religion}
-         city = {c.city} contact = {c.contact} favourite = {c.favourite} />)}
-       </div>
-     );
-    }
-    else {
-      return (
-        <div className="section-label">
-          No result found!
-        </div>
-      );
-    }
-   }
- }
-
-class Profile extends React.Component {
-  render() {
-    return (
-    <div className="profile">
-      <img src={this.props.photo} className="img-circular" alt="profile-img"/>
-      <div className="display-job">
-        <span className="content">{this.props.displayName}</span>
-        <span className="job-title">{this.props.job}</span>
-      </div>
-      <div className="extra-content">
-        <span className="content">{this.props.age}</span>
-        <span className="content">{this.props.height}</span>
-      </div>
-      <div className="extra-content">
-        <span className="content">{this.props.religion}</span>
-        <span className="content">{this.props.city}</span>
-      </div>
-      <div className="extra-content">
-        <span className="content">{this.props.contact}</span>
-        <span className="content">{this.props.favourite}</span>
-      </div>
-    </div>)
-  }
  }
 
  export default App;
---
+++ src/components/Profiles.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../style/App.css';
+
+class Profiles extends React.Component {
+    render() {
+      if(this.props.profiles.length !== 0) {
+       return (
+        <div className="feed">
+          {this.props.profiles.map(c => <Profile displayName={c.displayName} job={c.job}
+          age = {c.age} height = {c.height} photo = {c.photo} religion = {c.religion}
+          city = {c.city} contact = {c.contact} favourite = {c.favourite} />)}
+        </div>
+      );
+     }
+     else {
+       return (
+         <div className="section-label">
+           No result found!
+         </div>
+       );
+     }
+    }
+  }
+ 
+ class Profile extends React.Component {
+   render() {
+     return (
+     <div className="profile">
+       <img src={this.props.photo} className="img-circular" alt="profile-img"/>
+       <div className="display-job">
+         <span className="content">{this.props.displayName}</span>
+         <span className="job-title">{this.props.job}</span>
+       </div>
+       <div className="extra-content">
+         <span className="content">{this.props.age}</span>
+         <span className="content">{this.props.height}</span>
+       </div>
+       <div className="extra-content">
+         <span className="content">{this.props.religion}</span>
+         <span className="content">{this.props.city}</span>
+       </div>
+       <div className="extra-content">
+         <span className="content">{this.props.contact}</span>
+         <span className="content">{this.props.favourite}</span>
+       </div>
+     </div>)
+   }
+  }
+
+  export default Profiles;
+ 
