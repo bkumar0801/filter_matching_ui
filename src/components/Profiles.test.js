16e0ea0d1f9efd633dec323c2662d31bfe31d411
Bijendra Kumar <kumarbijendra@gmail.com>
2018-05-13 22:19:07 +0200

    Bijendra | Add profile test

---
+++ src/components/Profiles.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow, enzyme } from 'enzyme';
+import Profiles from './Profiles';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import {sinon, spy, stub} from 'sinon';
+
+configure({ adapter: new Adapter() });
+
+describe(Profiles, () => {
+    const mockOnInput = jest.fn();
+    const emptyProfiles = []
+    const profiles = [{
+        displayName: "Test",
+        job: "Engineer",
+        age: "Age: 18 yrs",
+        height: "Height: 100 cm",
+        photo: "test pic",
+        religion: "Religion: Hindu",
+        city: "City: Test",
+        contact: "Contacts Exchanged: 2",
+        favourite: "Favourite: True",
+        score: "Compatibility Score: 90%"
+      },
+    ];
+    const component = shallow(<Profiles profiles = {profiles} />); 
+
+    it('renders and matches our snapshot with empty profile', () => {
+        const component = renderer.create(<Profiles profiles = {emptyProfiles}/>);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders and matches our snapshot with profile', () => {
+        const component = renderer.create(<Profiles profiles = {profiles}/>);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
